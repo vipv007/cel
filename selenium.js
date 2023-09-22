24d049f3d26d303300c6e1ef65693e8c644b8257
DG4723 <95736757+DG4723@users.noreply.github.com>
2023-09-22 11:14:59 +0530

    selenium update

---
+++ selenium.js
@@ -0,0 +1,36 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+// Configure Chrome options if needed
+const options = new chrome.Options();
+
+
+async function runSeleniumTest() {
+  const driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
+
+  try {
+    // Open a webpage
+    await driver.get('https://example.com');
+
+    // Wait for the title to match the expected title
+    const expectedTitle = 'Hello world';
+    await driver.wait(until.elementLocated(By.xpath("//*[text()='Hello world']")), 5000); // Wait for the title to match
+
+    console.log('Test Passed! Title matches the expected value.');
+  } catch (error) {
+    console.error(`Test Failed! Error: ${error.message}`);
+  } finally {
+    // Close the browser window
+    await driver.quit();
+
+    // Stop the ChromeDriver server
+    chromedriver.stop();
+  }
+}
+
+// Call the function to run the Selenium test
+runSeleniumTest();
